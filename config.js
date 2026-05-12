a3a7837db1f4f7559c3109eaf88d509b04c7192e
fareskrichati <120513736+fareskrichati@users.noreply.github.com>
2026-05-12 16:54:10 -0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.DAILY_PLANNER_SUPABASE = {
-  url: "https://mgvahslbxdskzhiwxxod.supabase.com",
+  url: "https://mgvahslbxdskzhiwxxod.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ndmFoc2xieGRza3poaXd4eG9kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0NTE2MzksImV4cCI6MjA5NDAyNzYzOX0.ovt30I4ZqPclxcXR5XJVrtBKUn_bVz17vrTJklxg3h8",
 };
